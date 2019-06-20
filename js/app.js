c5dc888ad8bc2ce2d1b67c5eb4a8a538d8c64315
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-06-20 17:14:03 -0600

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,45 @@
+// 1. VERBAL QUESTIONS:
+/*
+Parameters is what is declared after the function variable. 
+Arguments are where you place the value 
+in what's being called. Arguments, after being given,
+will be placed in the  parameters.
+
+Within in function, the return if just saying what
+to do with the parameters. It doesn't ooutput anything. 
+Console.log shows what is happening in the code. 
+
+The implicationos of a 'return' returning a value 
+is that in a function the 'return' returns the value 
+of that function to be executed again. 
+When we console.log the function, it will return
+one of the return values. 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
